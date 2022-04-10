5d6b29ee8701bce4b29a79424ea7e63f6858b7f7
samaa <samaaehab48@gmail.com>
2022-04-10 22:22:50 +0200

    view getDetailes api

--- movies/src/app/componants/view/view.component.ts
+++ movies/src/app/componants/view/view.component.ts
@@ -1,4 +1,6 @@
+import { MovieService } from 'src/app/services/movie.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view.component.css']
 })
 export class ViewComponent implements OnInit {
-
-  constructor() { }
+  prodid:any;
+  constructor(private _activatedRoute:ActivatedRoute,private _movieDetails:MovieService) { }
 
   ngOnInit(): void {
+    
+    this._activatedRoute.paramMap.subscribe(params => {
+      this.prodid = params.get('pId');
+    });
+    this._movieDetails.getDetails(this.prodid).subscribe(
+      (res:any)=>{
+        console.log(res)
+      });
+    console.log(this.prodid)
+
   }
 
 }
